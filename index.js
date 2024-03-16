04c4b70262a7d29dbd286d80362feaa5f30d4e26
ngecu <devngecu@gmail.com>
2024-03-16 23:04:18 +0300

    initialized frontend

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-require('dotenv').config
 
+// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI("AIzaSyAfI1syH49uxIyfl2tAra-Fe7DadcELdxE");
 
 async function run() {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-    const prompt = "Write a story about a magic backpack."
-  
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-  }
-  
-//   run();
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: "Hello, I have 2 dogs in my house.",
+      },
+      {
+        role: "model",
+        parts: "Great to meet you. What would you like to know?",
+      },
+    ],
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  });
+
+  const msg = "How many paws are in my house?";
+
+  const result = await chat.sendMessage(msg);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
